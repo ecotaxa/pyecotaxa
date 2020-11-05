c2eb564102b80e839ce9de3a9aae982ac7aeabe9
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2020-11-05 19:22:53 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+with open("README.md", "r") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    name="pyecotaxa",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    # author="TODO",
+    # author_email=TODO",
+    description="Query EcoTaxa and process its output",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ecotaxa/pyecotaxa",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pandas"],
+    python_requires=">=3.6",
+    extras_require={
+        "tests": [
+            # Pytest
+            "pytest",
+            "pytest-cov",
+        ],
+        "docs": [
+            "sphinx >= 1.4",
+            "sphinx_rtd_theme",
+            "sphinx-autodoc-typehints>=1.10.0",
+        ],
+    },
+    entry_points={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+    ],
+)
